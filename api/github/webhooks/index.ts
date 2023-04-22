165752c723f03259c5aa42b36b3fd6dd485f4def
ysato <56924527+ysato@users.noreply.github.com>
2023-04-22 16:01:08 +0900

    Initial commit

---
+++ api/github/webhooks/index.ts
@@ -0,0 +1,20 @@
+import { createNodeMiddleware, createProbot } from 'probot';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import app from '../../../src';
+
+const middleware = createNodeMiddleware(app, {
+  probot: createProbot(),
+  webhooksPath: '/api/github/webhooks',
+});
+
+export = (request: VercelRequest, response: VercelResponse) => {
+  if (request.method !== 'POST') {
+    response.writeHead(302, {
+      Location: '/ping',
+    });
+    response.end();
+    return;
+  }
+
+  middleware(request, response);
+};
